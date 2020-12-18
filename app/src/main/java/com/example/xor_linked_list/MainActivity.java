082f44c643f53ba6ad29d10310c1f4acc965a4f7
isriom <62965230+isriom@users.noreply.github.com>
2020-12-17 22:04:22 -0600

    docummentations

--- app/src/main/java/com/example/xor_linked_list/MainActivity.java
+++ app/src/main/java/com/example/xor_linked_list/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.RelativeLayout;
 import com.example.LCDE.Lcde;
 
 public class MainActivity extends AppCompatActivity {
+//    Vista de la xortlist
     public XorView xorlist;
 
 
@@ -18,17 +19,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//        encontrar el layout actual
         RelativeLayout screen= (RelativeLayout) findViewById(R.id.layout1);
+//        encontrar la vista de la  xortlist del xml
         xorlist = (XorView) findViewById(R.id.Xor1);
+//        Agregarlo a al layout
         screen.addView(xorlist);
     }
 
     public void insert(View view){
+//        encontrar el layout actual
         RelativeLayout screen= (RelativeLayout) findViewById(R.id.layout1);
+//        Eliminar la lista que se ve actualmente
         screen.removeView(xorlist);
+//        Encontrar el cuadro de texto e insertar valor nuevo
         EditText text= (EditText) findViewById(R.id.Text);
         XorView.list.insert(Integer.parseInt(text.getText().toString()));
+//        agregar el nuevo display de la lista
         screen.addView(xorlist);
     }
     public void delete(View view){
--- app/src/main/java/com/example/xor_linked_list/XorView.java
+++ app/src/main/java/com/example/xor_linked_list/XorView.java
@@ -13,23 +13,33 @@ public class XorView extends View {
     public Paint black= new Paint();
     public XorView(Context context) {
         super(context);
+//        Crear lista vacia
         list= new Lcde();
+//        crear pincel negro
         black.setARGB(0,0,0,0);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+//        poner fondo negro
         canvas.drawRGB(255,255,255);
         Node actual=null;
+//        Conseguir el primer nodo
         if(list.getStart()!=null){
             actual=list.getStart();}
+//        Para cada nodo:
         for (int i=0;i<list.sizeLCDE;++i){
             if (actual!=null){
+//                dibujar cuadrado interior
                 canvas.drawRect(12*i,10,10+12*i,40,black);
+//                dibujar cuadrado exterior
                 canvas.drawRect(10*i,8,10+10*i,42,black);
+//                poner titulo con el valor
                 canvas.drawText(String.valueOf(actual.fact), (int)(((i * 12) + 8 + (12 * i)) / 2),25,black);
             }
+//           cambiar nodo
+            actual=actual.nextNode;
 
         }
 
